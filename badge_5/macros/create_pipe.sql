97f84e7c9672130b7e9640432b4f42d65a62eaa7
mtenaud <mtenaud@ippon.fr>
2023-06-26 09:57:12 +0200

    create pipe

---
+++ badge_5/macros/create_pipe.sql
@@ -0,0 +1,29 @@
+{% macro create_pipe(pipe_name, table_name, schema_name = target.schema, db_name = target.database, dry_run = True) %}
+
+    {% set sql %}
+        CREATE PIPE {{ db_name }}.{{ schema_name }}.{{pipe_name}}
+        auto_ingest=true
+        aws_sns_topic='arn:aws:sns:us-west-2:321463406630:dngw_topic'
+        AS 
+        COPY INTO {{table_name}}
+        FROM (
+            SELECT 
+                METADATA$FILENAME as log_file_name, 
+                METADATA$FILE_ROW_NUMBER as log_file_row_id,
+                current_timestamp(0) as load_ltz,
+                get($1,'datetime_iso8601')::timestamp_ntz as DATETIME_ISO8601,
+                get($1,'user_event')::text as USER_EVENT,
+                get($1,'user_login')::text as USER_LOGIN,
+                get($1,'ip_address')::text as IP_ADDRESS
+            FROM @uni_kishore_pipeline
+        )
+        file_format = (format_name = ff_json_logs);
+    {% endset %}
+
+    {% if not dry_run %}
+        {% do run_query(sql) %}
+    {% else %}
+        {{ log(sql, info=True) }}
+    {% endif %}
+
+{% endmacro %}
--- dags/test.py
+++ dags/test.py
@@ -13,10 +13,10 @@ default_args = {
 
 
 with DAG('2_Test_Badge_5', default_args=default_args, schedule_interval='@once') as dag:
-
-    task_test_stage = BashOperator(
-        task_id='test_create_stage',
-        bash_command='cd /dbt && dbt run --model ed_pipeline_logs',
+    task_seed = BashOperator(
+        task_id='load_seed_todl_once',
+        bash_command='cd /dbt && \
+            dbt seed --profiles-dir .',
         env={
             'dbt_user': '{{ var.value.dbt_user }}',
             'dbt_password': '{{ var.value.dbt_password }}',
@@ -25,5 +25,65 @@ with DAG('2_Test_Badge_5', default_args=default_args, schedule_interval='@once')
         dag=dag
     )
 
-task_test_stage
-# dbt run-operation create_file_format --args '{"file_format_name": "ff_json_logs", "file_format_type": "\"JSON\"", "strip_outer_array": true}'
+    task_stage = BashOperator(
+        task_id='create_stage',
+        bash_command='cd /dbt && \
+            dbt run-operation create_stage --args \'{\
+            "stage_name": "uni_kishore_pipeline", \
+            "stage_url": "s3://uni-kishore-pipeline", \
+            "dry_run": false}\'',
+        env={
+            'dbt_user': '{{ var.value.dbt_user }}',
+            'dbt_password': '{{ var.value.dbt_password }}',
+            **os.environ
+        },
+        dag=dag
+    )
+
+    task_file_format = BashOperator(
+        task_id='create_file_format',
+        bash_command='cd /dbt && \
+            dbt run-operation create_file_format --args \'{\
+                "file_format_name": "ff_json_logs", \
+                "file_format_type": "JSON", \
+                "strip_outer_array": true, \
+                "dry_run": false}\'',
+        env={
+            'dbt_user': '{{ var.value.dbt_user }}',
+            'dbt_password': '{{ var.value.dbt_password }}',
+            **os.environ
+        },
+        dag=dag
+    )
+
+    task_ed_pipeline_logs = BashOperator(
+        task_id='create_ed_pipeline_logs',
+        bash_command='cd /dbt && \
+            dbt run --model ed_pipeline_logs',
+        env={
+            'dbt_user': '{{ var.value.dbt_user }}',
+            'dbt_password': '{{ var.value.dbt_password }}',
+            **os.environ
+        },
+        dag=dag
+    )
+
+    task_pipe = BashOperator(
+        task_id='create_pipe',
+        bash_command='cd /dbt && \
+            dbt run-operation create_pipe --args \'{\
+            "pipe_name": "get_new_files", \
+            "table_name": "ed_pipeline_logs", \
+                "dry_run": false}\'',
+        env={
+            'dbt_user': '{{ var.value.dbt_user }}',
+            'dbt_password': '{{ var.value.dbt_password }}',
+            **os.environ
+        },
+        dag=dag
+    )
+
+task_stage >> task_ed_pipeline_logs
+task_file_format >> task_ed_pipeline_logs
+task_seed >> task_ed_pipeline_logs
+task_ed_pipeline_logs >> task_pipe
